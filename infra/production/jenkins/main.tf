91c68b586bdc3b8d30fc2426b83bd78744882f0f
jphetphoumy <j.phetphoumy@gmail.com>
2024-09-23 15:05:59 +0000

    Update jenkins role

--- infra/production/jenkins/main.tf
+++ infra/production/jenkins/main.tf
@@ -12,3 +12,18 @@ module "runick" {
 
   tags = ["linux", "cicd", "jenkins-master"]
 }
+
+module "dogmatika" {
+  source = "../../modules/lxc"
+
+  checksum = "39f6d06e082d6a418438483da4f76092ebd0370a91bad30b82ab6d0f442234d63fe27a15569895e34d6d1e5ca50319f62637f7fb96b98dbde4f6103cf05bff6d"
+  checksum_alg = "sha512"
+  filename = "debian-12-standard_12.7-1_amd64.tar.zst"
+  file_url = "http://download.proxmox.com/images/system/debian-12-standard_12.7-1_amd64.tar.zst"
+
+  vm_id = "221"
+  hostname = "dogmatika"
+  ip_address = "192.168.1.221/24"
+
+  tags = ["linux", "cicd", "jenkins-agent"]
+}
